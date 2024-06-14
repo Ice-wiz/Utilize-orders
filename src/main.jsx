ca70a5bfad034830df0d9d23d7db6855beba05b8
Aryan <img_2020012@iiitm.ac.in>
2024-06-14 21:26:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,12 @@ import { RecoilRoot } from "recoil";
 import App from "./App.jsx";
 import "./index.css";
 
+// Log all the environment variables
+console.log('VITE_KINDE_CLIENT_ID:', import.meta.env.VITE_KINDE_CLIENT_ID);
+console.log('VITE_KINDE_DOMAIN:', import.meta.env.VITE_KINDE_DOMAIN);
+console.log('VITE_KINDE_LOGOUT_URL:', import.meta.env.VITE_KINDE_LOGOUT_URL);
+console.log('VITE_KINDE_REDIRECT_URL:', import.meta.env.VITE_KINDE_REDIRECT_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <KindeProvider
